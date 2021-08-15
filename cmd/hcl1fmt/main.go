5b6a4f0274fbf8275b05515463a565be5b1ebc44
Max Bowsher <maxbowsher@gmail.com>
2021-08-15 11:56:39 +0100

    Add a simple CLI frontend for existing formatting logic

---
+++ cmd/hcl1fmt/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hashicorp/hcl/hcl/fmtcmd"
+)
+
+func main() {
+	var options fmtcmd.Options
+	var extensionsCSV string
+
+	flag.BoolVar(&options.List, "list", false,
+		"List filenames in need of formatting")
+	flag.BoolVar(&options.Write, "write", false,
+		"Write reformatted content back to files in place")
+	flag.BoolVar(&options.Diff, "diff", false,
+		"Display diff of formatting changes")
+	flag.StringVar(&extensionsCSV, "extensions", "hcl", "File extensions (comma separated) to operate on when processing a directory")
+
+	extensions := strings.Split(extensionsCSV, ",")
+
+	flag.Parse()
+
+	err := fmtcmd.Run(flag.Args(), extensions, os.Stdin, os.Stdout, options)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
